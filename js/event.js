7198cbe7ec117346f9743da2d4039cd47028f2c8
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2012-09-16 01:55:24 +0200

    Added twitter button in post sidebar + auto scroll for sidebar in post page

--- js/event.js
+++ js/event.js
@@ -20,5 +20,32 @@
             return true;
         });
 
+        // Sidebar
+        if ($('.post').length) {
+            var timer = 1000;
+            var sideWrapTop = $('#sideWrap')
+                .css('position', 'relative')
+                .offset()
+                .top;
+            var commentsTop = $('#comments')
+                .position()
+                .top;
+            var sidebarScroll = function () {
+                var pos = $('#sideWrap').offset();
+                var scroll = $('html').scrollTop();
+                if (scroll > sideWrapTop) {
+                    if (scroll > commentsTop) {
+                        $('#sideWrap').animate({'top': commentsTop - sideWrapTop}, 200);
+                    } else {
+                        $('#sideWrap').animate({'top': scroll - sideWrapTop}, 200);
+                    }
+                } else if (scroll < sideWrapTop && pos.top != sideWrapTop) {
+                    $('#sideWrap').animate({'top': 0}, 200);
+                }
+                setTimeout(sidebarScroll, timer);
+            }
+            setTimeout(sidebarScroll, timer);
+        }
+
     });
 })(jQuery);
