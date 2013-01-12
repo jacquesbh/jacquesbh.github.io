05cc140fdab60c5265534f8677c78758e553d783
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2013-01-13 00:17:40 +0100

    Fix safari javascript bug

--- js/functions.js
+++ js/functions.js
@@ -2,11 +2,11 @@
 /**
  * Gist object
  */
-var gist = function (gistNumber, file, lines = null)
+var gist = function (gistNumber, file, lines)
 {
     document.write('<script src="https://gist.github.com/'+gistNumber+'.js?file='+file+'"></script>');
 
-    if (lines != null) {
+    if (lines != undefined && lines != null) {
         var id = '#gist' + gistNumber + ' #file-' + file.replace('.', '-') + '-LC%d';
         var style = $('style[for=gists]');
         parts = lines.split(',');
